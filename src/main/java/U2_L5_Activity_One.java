270474a79e04101fa5a40fdf484747f0d25937b2
101zh <67253838+101zh@users.noreply.github.com>
2023-09-26 15:16:02 -0700

    Update U2_L5_Activity_One.java
    No access to library from projectStem

--- src/main/java/U2_L5_Activity_One.java
+++ src/main/java/U2_L5_Activity_One.java
@@ -1,18 +1,18 @@
-/* Lesson 5 Coding Activity Question 1 */
+// /* Lesson 5 Coding Activity Question 1 */
 
-import shapes.*;
+// import shapes.*;
 
-public class U2_L5_Activity_One
-{
-  public static void main(String[] args)
-  {
+// public class U2_L5_Activity_One
+// {
+//   public static void main(String[] args)
+//   {
 
-    Circle c1 = new Circle(10.1);
-    System.out.println(c1);
-    Circle c2 = new Circle(14.0);
-    System.out.println(c2);
-    Circle c3 = new Circle(20.5);
-    System.out.println(c3);
+//     Circle c1 = new Circle(10.1);
+//     System.out.println(c1);
+//     Circle c2 = new Circle(14.0);
+//     System.out.println(c2);
+//     Circle c3 = new Circle(20.5);
+//     System.out.println(c3);
 
-  }
-}
+//   }
+// }
