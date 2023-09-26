7c667d8839d35258a0980577f2a1728b79825085
101zh <67253838+101zh@users.noreply.github.com>
2023-09-26 15:18:25 -0700

    feat(2.5.2): finished

---
+++ src/main/java/U2_L5_Activity_Two.java
@@ -0,0 +1,24 @@
+// /* Lesson 5 Coding Activity Question 2 */
+
+// import java.util.Scanner;
+// import shapes.*;
+
+// public class U2_L5_Activity_Two
+// {
+//   public static void main(String[] args)
+//   {
+//     Scanner scanner = new Scanner(System.in);
+    
+//     System.out.println("Type a number for length and width:");
+//     Double sidelen=scanner.nextDouble();
+//     System.out.println("Type a length:");
+//     Double len = scanner.nextDouble();
+//     System.out.println("Type a width:");
+//     Double wid = scanner.nextDouble();
+//     Rectangle rect1 = new Rectangle(sidelen);
+//     Rectangle rect2 = new Rectangle(len, wid);
+//     System.out.println(rect1);
+//     System.out.println(rect2);
+    
+//   }
+// }
