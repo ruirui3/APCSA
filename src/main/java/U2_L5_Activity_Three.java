602f82e3b06a494d8d504a2f86f5ae7dd0436cda
101zh <67253838+101zh@users.noreply.github.com>
2023-09-26 15:17:27 -0700

    feat(2.5.3): finished

---
+++ src/main/java/U2_L5_Activity_Three.java
@@ -0,0 +1,19 @@
+// /* Lesson 5 Coding Activity Question 3 */
+
+// import java.util.Scanner;
+// import shapes.*;
+
+// public class U2_L5_Activity_Three
+// {
+//   public static void main(String[] args)
+//   {
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.println("Type a side length:");
+//     Double sidelen = scanner.nextDouble();
+//     RegularPolygon tri = new RegularPolygon(3,sidelen);
+//     RegularPolygon square = new RegularPolygon(4,sidelen);
+//     System.out.println(tri);
+//     System.out.println(square);
+
+//   }
+// }
