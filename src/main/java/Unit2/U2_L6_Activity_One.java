9fd8aeaa1fc4476d95d4962e746349c0e196fcb8
101zh <67253838+101zh@users.noreply.github.com>
2023-09-27 19:31:53 +0000

    feat(2.6): Finished Unit 2 Lesson 6 activities

---
+++ src/main/java/Unit2/U2_L6_Activity_One.java
@@ -0,0 +1,18 @@
+// /* Lesson 6 Coding Activity Question 1 */
+
+// import java.util.Scanner;
+// import shapes.*;
+
+// public class U2_L6_Activity_One
+// {
+//   public static void main(String[] args)
+//   {
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.println("Enter the radius of the circle:");
+//     System.out.print("> ");
+//     Double radius = scanner.nextDouble();
+//     Circle circ = new Circle(radius);
+//     System.out.println("A circle with a radius of "+circ.getRadius()+" has a circumference of "+circ.getCircumference()+" and an area of "+circ.getArea());
+
+//   }
+// }
---
+++ src/main/java/Unit2/U2_L6_Activity_Two.java
@@ -0,0 +1,25 @@
+// /* Lesson 6 Coding Activity Question 2 */
+
+// import java.util.Scanner;
+// import shapes.*;
+
+// public class U2_L6_Activity_Two
+// {
+//   public static void main(String[] args)
+//   {
+
+//     Scanner scanner = new Scanner(System.in);
+//     System.out.println("Enter number of sides:");
+//     System.out.print("> ");
+//     int sides = scanner.nextInt();
+//     System.out.println("Enter the side lengths:");
+//     System.out.print("> ");
+//     double length = scanner.nextDouble();
+//     RegularPolygon poly = new RegularPolygon(sides, length);
+//     System.out.println("Area with "+poly.getNumSides()+" sides: "+poly.getArea());
+//     System.out.println("Incrementing the number of sides by two");
+//     poly.addSides(2);
+//     System.out.println("Area with "+poly.getNumSides()+" sides: "+poly.getArea());
+
+//   }
+// }
