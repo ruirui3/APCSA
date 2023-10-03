dd11072441b015ecdb2d165c4d4cdb8efe95bb16
101zh <67253838+101zh@users.noreply.github.com>
2023-10-03 03:57:49 +0000

    feat(SubstringTest): testing out substring method

---
+++ src/test/java/SubstringTest.java
@@ -0,0 +1,24 @@
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SubstringTest {
+    @Test
+    public void zeroZeroTest(){
+        String a="a";
+        a=a.substring(0, 0);
+        assertTrue(a.equals(""));
+    }
+
+    @Test
+    public void lengthTest(){
+        String a = "012345";
+        String segment = "23";
+        int index = a.indexOf(segment);
+        a = a.substring(0, index)+a.substring(index+segment.length());
+        assertTrue(a.equals("0145"));
+
+    }
+}
