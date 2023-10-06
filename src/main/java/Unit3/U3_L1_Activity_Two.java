17c3a0d647925addeb66ec11dba22bfb41e1e3cd
101zh <67253838+101zh@users.noreply.github.com>
2023-10-06 20:55:40 +0000

    feat(3.1.2): Finished U3_L1_Activity_Two

---
+++ src/main/java/Unit3/U3_L1_Activity_Two.java
@@ -0,0 +1,15 @@
+package Unit3;
+
+import java.util.Scanner;
+
+public class U3_L1_Activity_Two {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int x = scan.nextInt();
+        if (x == 48) {
+            System.out.println("YES");
+        }
+        scan.close();
+
+    }
+}
