d7990a02a2ca7947c618760165c93b68694390a4
101zh <67253838+101zh@users.noreply.github.com>
2023-10-09 20:39:45 +0000

    feat(3.2.2): Finished U3_L2_Activity_Two

---
+++ src/main/java/Unit3/U3_L2_Activity_Two.java
@@ -0,0 +1,21 @@
+package Unit3;
+
+import java.util.Scanner;
+
+public class U3_L2_Activity_Two {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double num1 = scanner.nextDouble();
+        double num2 = scanner.nextDouble();
+
+        if (num1 > num2) {
+            System.out.println(num1);
+        } else {
+            System.out.println(num2);
+        }
+        // System.out.println(Math.min(num1, num2));
+
+        scanner.close();
+    }
+}
