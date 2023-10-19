98e1c3cc01e96739691e2fe2e8764c2ad6f531b5
101zh <67253838+101zh@users.noreply.github.com>
2023-10-19 03:39:21 +0000

    feat(3.7.3): Finished U3_L7_Activity_Three

---
+++ src/main/java/Unit3/U3_L7_Activity_Three.java
@@ -0,0 +1,27 @@
+// package Unit3;
+
+// import java.util.Scanner;
+// // import shapes.*;
+
+// public class U3_L7_Activity_Three {
+//     public static void main(String[] args){
+//         Scanner scanner = new Scanner(System.in);
+//         System.out.println("Enter the side length of the first regular polygon:");
+//         double sidelen1 = scanner.nextDouble();
+//         System.out.println("Enter the number of sides of the second polygon:");
+//         int sidenum2 = scanner.nextInt();
+//         System.out.println("Enter the side length of the second polygon:");
+//         double sidelen2 = scanner.nextDouble();
+
+//         RegularPolygon poly1 = new RegularPolygon(sidelen1);
+//         RegularPolygon poly2 = new RegularPolygon(sidenum2, sidelen2);
+
+//         if(poly1.equals(poly2)){
+//             System.out.println("Congruent Regular Polygons!");
+//         } else{
+//             System.out.println("Different Regular Polygons");
+//         }
+
+//         scanner.close();
+//     }
+// }
