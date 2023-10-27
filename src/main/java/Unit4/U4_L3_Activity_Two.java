d24dbca98e0d0cfb5a87cc0cadec278367b00945
101zh <67253838+101zh@users.noreply.github.com>
2023-10-27 23:39:19 +0000

    feat(4.3.2): Finish U4_L3_Activity_Two

---
+++ src/main/java/Unit4/U4_L3_Activity_Two.java
@@ -0,0 +1,12 @@
+package Unit4;
+
+public class U4_L3_Activity_Two {
+    public static void main(String[] args) {
+        for (int i=17; i<=73; i++){
+            System.out.print(i+" ");
+            if((i-6)%10==0){
+                System.out.println();
+            }
+        }
+    }
+}
