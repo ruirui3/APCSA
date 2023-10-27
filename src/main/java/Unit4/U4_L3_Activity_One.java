8080f0f8cd44f1d2554a2d6049d5e0fe2428cf23
101zh <67253838+101zh@users.noreply.github.com>
2023-10-27 23:31:33 +0000

    feat(4.3.1): Finish U4_L3_Activity_One

---
+++ src/main/java/Unit4/U4_L3_Activity_One.java
@@ -0,0 +1,9 @@
+package Unit4;
+
+public class U4_L3_Activity_One {
+    public static void main(String[] args) {
+        for (int i = 1; i<=25; i+=2){
+            System.out.println(i);
+        }
+    }
+}
