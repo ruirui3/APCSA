519261287cffe4e86093ab6716fec411195f4f71
101zh <67253838+101zh@users.noreply.github.com>
2023-10-30 20:28:59 +0000

    fix(4.3.2): remove unnecessary operation

--- src/main/java/Unit4/U4_L3_Activity_Two.java
+++ src/main/java/Unit4/U4_L3_Activity_Two.java
@@ -4,7 +4,7 @@ public class U4_L3_Activity_Two {
     public static void main(String[] args) {
         for (int i=17; i<=73; i++){
             System.out.print(i+" ");
-            if((i-6)%10==0){
+            if(i%10==6){
                 System.out.println();
             }
         }
