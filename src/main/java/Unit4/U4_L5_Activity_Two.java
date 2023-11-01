b1b70f6d05a32c2ebf6da128df006fa62f172d72
101zh <67253838+101zh@users.noreply.github.com>
2023-11-01 21:59:53 +0000

    feat(4.5.2): Finish U4_L5_Activity_Two

---
+++ src/main/java/Unit4/U4_L5_Activity_Two.java
@@ -0,0 +1,16 @@
+package Unit4;
+
+public class U4_L5_Activity_Two {
+
+    public static void main(String[] args) {
+        
+        for (int i = 10; i>=1; i--){
+            for (int j=1; j<=i; j++){
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+
+    }
+}
+
