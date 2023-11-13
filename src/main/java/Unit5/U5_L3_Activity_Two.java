65459281fa2f716c4412d698349877a8d4cea1ae
101zh <67253838+101zh@users.noreply.github.com>
2023-11-13 21:49:03 +0000

    feat(5.3): finish unit 5 lesson 3 activities

---
+++ src/main/java/Unit5/U5_L3_Activity_Two.java
@@ -0,0 +1,40 @@
+// /* Lesson 3 Coding Activity Question 2 */
+
+// import java.util.Scanner;
+// import shapes.*;
+// import testing.Math;
+
+// public class U5_L3_Activity_Two {
+
+//     /* Add the method randomize here */
+
+//     // You can uncomment and add to the main method to test your code
+//     // You will need to remove/comment out this method before submitting your code
+
+//     public static void randomize(Rectangle rect) {
+//         int width = ((int) (Math.random() * 4) * 2) + 7;
+//         int length = ((int) (Math.random() * 6) * 2) + 10;
+
+//         rect.setWidth(width);
+//         rect.setLength(length);
+//     }
+    
+//     /*
+//     public static void main(String[] args) {
+//         Rectangle rect = new Rectangle();
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//         randomize(rect);
+//         System.out.println(rect);
+//     }
+//     */
+// }
