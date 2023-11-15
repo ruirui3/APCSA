20040c7e0196c24598c0f91a8f1f105398ed2bb2
101zh <67253838+101zh@users.noreply.github.com>
2023-11-15 01:50:59 +0000

    feat(5.4): finish unit 5 lesson 4 activities

---
+++ src/main/java/Unit5/U5_L4_Activity_One.java
@@ -0,0 +1,23 @@
+// /* Lesson 4 Coding Activity Question 1 */
+
+// import java.util.Scanner;
+// import shapes.*;
+
+// public class U5_L4_Activity_One {
+
+//     /* Add the method totalSides here */
+
+//     // You can uncomment and add to the main method to test your code
+//     // You will need to remove/comment out this method before submitting your code
+
+//     public static int totalSides(RegularPolygon p1, RegularPolygon p2) {
+//         int sides = p1.getNumSides() + p2.getNumSides();
+//         return sides;
+//     }
+
+//     /*
+//      * public static void main(String[] args){
+//      * 
+//      * }
+//      */
+// }
