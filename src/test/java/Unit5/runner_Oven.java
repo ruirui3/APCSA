3670e325e5c57d890d4da4aad38b806f4f8e84f1
101zh <67253838+101zh@users.noreply.github.com>
2023-11-16 03:32:00 +0000

    chore(5.5.2): format & close scanner
    - change runner_Oven.java

--- src/test/java/Unit5/runner_Oven.java
+++ src/test/java/Unit5/runner_Oven.java
@@ -2,11 +2,9 @@ package Unit5;
 
 import java.util.Scanner;
 
-public class runner_Oven 
-{
+public class runner_Oven {
 
-  public static void main(String[] args) 
-  {
+  public static void main(String[] args) {
     Scanner scan = new Scanner(System.in);
     System.out.println("Maximum oven temperature:");
     int maxTemp = scan.nextInt();
@@ -17,27 +15,26 @@ public class runner_Oven
 
     String instruction = "";
 
-    while (!instruction.equals("q")) 
-    {
-      System.out.println("New oven with a maximum temperature of " + oven.getMaxTemp() + " and a starting temperature of " +  oven.getCurrentTemp() + " degrees.");
+    while (!instruction.equals("q")) {
+      System.out.println("New oven with a maximum temperature of " + oven.getMaxTemp()
+          + " and a starting temperature of " + oven.getCurrentTemp() + " degrees.");
 
-      System.out.println("To preheat the oven enter \"p\", to turn the oven off enter" + " \"o\", to restart enter \"r\", to quit enter \"q\"");
+      System.out.println("To preheat the oven enter \"p\", to turn the oven off enter"
+          + " \"o\", to restart enter \"r\", to quit enter \"q\"");
 
       instruction = scan.next();
 
-      if (instruction.equals("p")) 
-      {
+      if (instruction.equals("p")) {
         System.out.println("Enter the temperature to preheat the oven to:");
         int temp = scan.nextInt();
         oven.preheat(temp);
         System.out.println("Current temperature of the oven is now " + oven.getCurrentTemp() + " degrees\n");
-      } 
-      else if (instruction.equals("o")) 
-      {
+      } else if (instruction.equals("o")) {
         System.out.println("Turning the oven off.\n");
         oven.turnOff();
       }
     }
+
+    scan.close();
   }
 }
-
