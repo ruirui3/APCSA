8408cade604d83eb78f8602e94ae1c80cc809739
101zh <67253838+101zh@users.noreply.github.com>
2023-12-06 20:36:24 +0000

    fix(6): Array Algorithms; do not return int[]
    - Arrays are reference types, so changes made to them are remembered

--- src/test/java/ArrayAlgorithms.java
+++ src/test/java/ArrayAlgorithms.java
@@ -1,28 +1,24 @@
 
 public class ArrayAlgorithms {
 
-    public static int[] shift(int[] list) {
+    public static void shift(int[] list) {
         int first = list[0];
         for (int i = 0; i < list.length - 1; i++) {
             list[i] = list[i + 1];
         }
         list[list.length - 1] = first;
 
-        return list;
-
     }
 
-    public static int[] insert(int[] list, int val, int loc) {
+    public static void insert(int[] list, int val, int loc) {
         for (int i = list.length - 1; i > loc; i--) {
             list[i] = list[i - 1];
         }
         list[loc] = val;
 
-        return list;
-
     }
 
-    public static int[] reverse(int[] list) {
+    public static void reverse(int[] list) {
         for (int i = 0; i < list.length / 2; i++) {
             int temp = list[i];
             int j = list.length - 1 - i;
@@ -30,16 +26,12 @@ public class ArrayAlgorithms {
             list[j] = temp;
         }
 
-        return list;
-
     }
 
-    public static int[] delete(int[] list, int index) {
+    public static void delete(int[] list, int index) {
         for (int i = index; i < list.length - 1; i++) {
             list[i] = list[i + 1];
         }
-
-        return list;
 
     }
 
