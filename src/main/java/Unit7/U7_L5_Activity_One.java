a4fa0798307fc8d2a49e47f7418d5d02e74d297f
101zh <67253838+101zh@users.noreply.github.com>
2024-01-09 21:30:17 +0000

    feat(7.5): finish unit 7 lesson 5 activities

---
+++ src/main/java/Unit7/U7_L5_Activity_One.java
@@ -0,0 +1,23 @@
+package Unit7;
+
+// Have to get rid of package statement
+
+public class U7_L5_Activity_One {
+    public static void sortAndPrintReverse(String[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            int high = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j].compareTo(arr[high]) > 0) {
+                    high = j;
+                }
+            }
+            String temp = arr[i];
+            arr[i] = arr[high];
+            arr[high] = temp;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
---
+++ src/main/java/Unit7/U7_L5_Activity_Two.java
@@ -0,0 +1,30 @@
+package Unit7;
+
+// Have to get rid of package statement
+
+import java.util.ArrayList;
+
+public class U7_L5_Activity_Two
+{
+
+  public static void selectSortReverse(ArrayList<Integer> list){
+    
+    for(int i=0; i<list.size(); i++){
+      int largestIndex=i;
+      for(int j=i+1; j<list.size(); j++){
+        if(list.get(j)>list.get(largestIndex)){
+          largestIndex=j;
+        }
+      }
+      int temp = list.get(i);
+      
+      list.set(i, list.get(largestIndex));
+      list.set(largestIndex, temp);
+    }
+    
+    System.out.println(list);
+    
+  }
+
+}
+
